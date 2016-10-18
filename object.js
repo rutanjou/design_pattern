81e6e6ce2f1cc4a93333b2854b4b035d92b86974
Ru Tan <jutan2012@gmail.com>
2016-10-18 14:42:45 +0200

    Ajout button valider

--- object.js
+++ object.js
@@ -1,59 +1,63 @@
 (function(){
   window.app={
-    timer:50,
+    timer:null,
     intervalID:null,
+    min:null,
+    secon:null,
     init:function(){
       app.listeners();
     },
     listeners:function(){
-      $("#play").on('click',app.play);
-      $("#stop").on('click',app.stop);
-      $("#reset").on('click',app.reset);
-      $("#reini").on('click',app.reini);
-      $("#iniminut").val();
-      $("#inisecond").val();
-      
+      $("#play").on('click',this.play);
+      $("#stop").on('click',this.stop);
+      $("#reset").on('click',this.reset);
+      $("#reini").on('click',this.reini);
+    
     },
     updateView:function(){
-      var minute = parseInt(app.timer/60);
-      var second = parseInt(app.timer-minute*60);
+      var minute = parseInt(app.timer/60,10);
+      var second = parseInt(app.timer-minute*60,10);
 
       $("#minute").text(minute);
       $("#second").text(second);
     },
     play: function(){
-      app.intervalID = setInterval(app.decrement,1000);
-      
+      app.intervalID = setInterval(app.decrement,1000); 
+      if(app.timer === 0)
+      {
+       app.stop();
+      };  
     },
     
     decrement:function(){
       app.timer--;
       app.updateView();
-
-          if(app.timer === 0)
+      if(app.timer === 0)
       {
         app.stop();
       };
     },
     stop:function(){
-      clearInterval(app.intervalID);
+        clearInterval(app.intervalID);
+        if(app.timer === 0)
+      {
+        clearInterval(app.intervalID);
+      };
     },
     reset:function(){
-      app.timer = 50;  
+      app.timer = 10;
     },
-    reini:function{
-        
+    reini:function(){
+      app.min = parseInt($('#iniminut').val(),10);
+      app.secon = parseInt($('#inisecond').val(),10);
+      app.updateView();
+      app.timer = parseInt(app.min,10)*60 + parseInt(app.secon,10);
+      if(app.timer === 0)
+      {
+        clearInterval(app.intervalID);
+      };
     },
-
-
-    userSetTime: function(){
-      var second= parseInt($('#second').val(),1000);
-      app.setTimeout(second);
-
-    },
-    setTimeout:function(temp){
-      app.temps=temp;
-    }
+    
   };
   app.init();
 })();

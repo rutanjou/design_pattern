6ab880b72677a8c95d483a79a83780be0b43205e
Ru Tan <jutan2012@gmail.com>
2016-10-19 16:51:31 +0200

    Ajout function barreprogress

--- object.js
+++ object.js
@@ -4,37 +4,33 @@
     intervalID:null,
     min:null,
     secon:null,
+
+    max:null,
     init:function(){
       app.listeners();
     },
     listeners:function(){
-      $("#play").on('click',this.play);
-      $("#stop").on('click',this.stop);
-      $("#reset").on('click',this.reset);
-      $("#reini").on('click',this.reini);
+      $("#play").on('click',app.play);
+      $("#stop").on('click',app.stop);
+      $("#reset").on('click',app.reset);
+      $("#reini").on('click',app.reini);
     
     },
-    updateView:function(){
-      var minute = parseInt(app.timer/60,10);
-      var second = parseInt(app.timer-minute*60,10);
-
-      $("#minute").text(minute);
-      $("#second").text(second);
-    },
     play: function(){
-      clearInterval(app.intervalID);
-      app.reini();
+    
       app.intervalID = setInterval(app.decrement,1000); 
      
     },
     
     decrement:function(){
       app.timer--;
+      app.progress();
       app.updateView();
-      if(app.timer === 0)
-      {
+      if(app.timer === 0){
         clearInterval(app.intervalID);
       };
+      
+
     },
     stop:function(){
         clearInterval(app.intervalID);
@@ -42,6 +38,7 @@
     },
     reset:function(){
       app.reini();
+      
 
     },
     reini:function(){
@@ -49,8 +46,27 @@
       app.secon = parseInt($('#inisecond').val(),10);
       app.updateView();
       app.timer = parseInt(app.min,10)*60 + parseInt(app.secon,10);
+      app.max=app.timer;
       
     }, 
+
+    
+    updateView:function(){
+      var minute = parseInt(app.timer/60,10);
+      var second = parseInt(app.timer-minute*60,10);
+
+      $("#minute").text(minute);
+      $("#second").text(second);
+    },
+    progress:function(){
+      var pro=$("#barre");
+      
+      var pourcentage = app.timer*100/app.max;
+    
+      pro.val(pourcentage);
+      
+    },
+    
   };
   app.init();
 
